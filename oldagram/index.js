26ab91b7808225d2f44dab331a8b66a6d13dd2dc
mr-qadeer <91522877+mr-qadeer@users.noreply.github.com>
2023-03-21 15:40:55 +0500

    Add files via upload

---
+++ oldagram/index.js
@@ -0,0 +1,75 @@
+
+
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "avatar-vangogh.jpg",
+        post: "post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "avatar-courbet.jpg",
+        post: "post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "avatar-ducreux.jpg",
+        post: "post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+// console.log(posts)
+// const post = document.getElementById('posts');
+
+// let html = "";
+
+
+// posts.map((post) => {
+//     html += `
+//         <div class="post" id="post">
+//                     <div class="avatar">
+//                         <img class="avatar-icon" src="${post.avatar}}">
+//                         <div class="avatar-info">
+//                             <p class="avatar-name">${post.name}</p>
+//                             <p class="avatar-location">${post.location}</p>
+//                         </div>
+//                     </div>
+                    
+//                     <div class="avatar-post">
+//                         <img class="post-image" src="${post.post}">
+//                     </div>
+//                     <div class="avatar-reactions">
+//                         <img class="like icon" src="icon-heart.png">
+//                         <img class="comment icon" src="icon-comment.png">
+//                         <img class="dm icon" src="icon-dm.png">
+//                     </div>
+//                     <div class="likes">
+//                         <p><span>${post.likes} </span>likes</p>
+//                     </div>
+//                     <p class="username">${post.username} <span class="caption">${post.comment}</span></p>
+//                 </div>
+//     `
+// })
+
+// // console.log(html)
+
+// post.innerHTML = html;
+
+
+
+
+
+
+
+
